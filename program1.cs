b6ac8b538e5f408b01d1c69effad419889e2adc3
Uzair Mohammed <70796064+Pizzarrific@users.noreply.github.com>
2025-07-30 20:42:16 +0530

    none

--- program1.cs
+++ program1.cs
@@ -1,12 +1,10 @@
 public class Problem1{  
-         public static bool ContainsDuplicate(int[] p1){
+         public static bool dupe(int[] p1){
         for (int i=0;i<p1.Length;i++){
             for (int j=i+1;j<p1.Length;j++){
                 if (p1[i]==p1[j]){
                     return true;
-                }
-            }
-        }
+                }}}
         return false;
     }
 }
--- program1_test.cs
+++ program1_test.cs
@@ -5,8 +5,8 @@ class TestProblem1{
         int[][] test = {
             new int[] {1,2,3,-1},new int[] {1,2,-3,4},new int[] {1,1,1,3,3,4,3,2,4,2},new int[] {0,0,0,1}};
         foreach(var arr in test){
-            bool result = Problem1.ContainsDuplicate(arr);
-            Console.WriteLine("[" + string.Join(",", arr) + "] - " + result);
+            bool result = Problem1.dupe(arr);
+            Console.WriteLine("[" + string.Join(",", arr)+"]-"+result);
 
         }
     }
