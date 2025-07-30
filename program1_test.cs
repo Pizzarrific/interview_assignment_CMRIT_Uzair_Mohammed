1d9c207fbeca67363d837650d8a32bfa772d57c6
Uzair Mohammed <70796064+Pizzarrific@users.noreply.github.com>
2025-07-30 19:40:36 +0530

    coommit

---
+++ program1_test.cs
@@ -0,0 +1,13 @@
+using System;
+
+class TestProblem1{
+    static void Main(){
+        int[][] testArrays = {
+            new int[] {1,2,3,-1},new int[] {1,2,-3,4},new int[] {1,1,1,3,3,4,3,2,4,2},new int[] {0,0,0,1}
+        };
+        foreach(var arr in testArrays){
+            bool result = Problem1.ContainsDuplicate(arr);
+            Console.WriteLine("[{0}]-{1}",string.Join(",",arr),result? "true":"false");
+        }
+    }
+}
