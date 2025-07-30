c54cc9d0bf8e230904fcd0bcf1b5bfcf53d65642
Uzair Mohammed <70796064+Pizzarrific@users.noreply.github.com>
2025-07-30 21:26:03 +0530

    none

--- program2.py
+++ program2.py
@@ -3,5 +3,10 @@ def program2(a):
   for j in range(len(a)-1):
    if a[j]==0 and a[j+1]!=0:
     a[j],a[j+1] = a[j+1],a[j]
+test=[0,1,0,4,21]
+for i in test:
+ program2(i)
+ print(i)
 
+ 
 
--- program2_test.py
+++ program2_test.py
@@ -1,3 +1,8 @@
+def program2(a):
+ for i in range(len(a)):
+  for j in range(len(a)-1):
+   if a[j]==0 and a[j+1]!=0:
+    a[j],a[j+1] = a[j+1],a[j]
 test=[
  [0,1,0,3,12],
  [1,2,3,4],
