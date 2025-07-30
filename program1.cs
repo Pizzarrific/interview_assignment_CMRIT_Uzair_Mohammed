3a2b590bedae10c9ac983996c64b2f94a883bbb9
Uzair Mohammed <70796064+Pizzarrific@users.noreply.github.com>
2025-07-30 20:20:10 +0530

    none

--- program1.cs
+++ program1.cs
@@ -1,7 +1,7 @@
 public class Problem1{  
          public static bool ContainsDuplicate(int[] p1){
-        for (int i=0;i<p1.Length; i++){
-            for (int j=i + 1;j<p1.Length; j++){
+        for (int i=0;i<p1.Length;i++){
+            for (int j=i+1;j<p1.Length;j++){
                 if (p1[i]==p1[j]){
                     return true;
                 }
